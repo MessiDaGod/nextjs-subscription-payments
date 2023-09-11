32dd0701a04dfd6c3b21469431800194f408afb2
joeshakely <joeshakely@gmail.com>
2023-09-10 21:52:13 -0700

    ok

--- components/ui/LoadingDots/LoadingDots.jsx
+++
@@ -1,9 +0,0 @@
-import s from './LoadingDots.module.css';
-var LoadingDots = function () {
-    return (<span className={s.root}>
-      <span />
-      <span />
-      <span />
-    </span>);
-};
-export default LoadingDots;
